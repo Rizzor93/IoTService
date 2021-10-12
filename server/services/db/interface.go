e40ea7d62248924cf74f129c32b35672f787b443
christoph <chris.bortolot@icloud.com>
2021-10-12 20:50:11 +0200

    Correct function "UpdateRecord"

--- server/services/db/interface.go
+++ server/services/db/interface.go
@@ -5,7 +5,10 @@ import (
 	"IoT_Service/server/services/db/models"
 	"fmt"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 // Handler handles all DB function
